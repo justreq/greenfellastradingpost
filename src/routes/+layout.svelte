2f03a564be24280debf61f986351916c9aef0c02
Arnimox <justreqofficial@gmail.com>
2025-04-16 23:15:00 -0400

    Update Dima's user ID

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -7,7 +7,7 @@
 	import { onMount } from "svelte";
 	let { children } = $props();
 
-	const greenfellas = ["2b5c3202-96ca-4ce2-b9dc-b4c4661e6f7b", "7fd134b3-8f07-4b91-b09e-2ffe3040dddf" "ca860e56-c5ae-4cb2-8b47-7195c5ee17f4", "d7f62e3e-9dad-4551-9aff-5ad5e24c3577"];
+	const greenfellas = ["2b5c3202-96ca-4ce2-b9dc-b4c4661e6f7b", "bb6b222d-c0c1-426e-bd7e-447c5089f675", "ca860e56-c5ae-4cb2-8b47-7195c5ee17f4", "d7f62e3e-9dad-4551-9aff-5ad5e24c3577"];
 
 	const moveParallaxBG = (event: Event) => {
 		let bg1 = document.getElementById("bg-1");
